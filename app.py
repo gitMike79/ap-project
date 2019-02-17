e74c8f277fefcac0e775e6bc4705ed22c72a3248
gitMike79 <mfrancoles@gmail.com>
2019-02-16 21:31:24 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+if __name__== '__main__':
+    app.run(debug=True)
