4a3f5f994927fea397622744334c01b9edcbc945
Ammar Eltigani <ammar170501@hotmail.com>
2019-02-17 20:01:23 -0500

    added todo(s) and fixed password authentication method in LogIn()

--- CreateAccount.py
+++ CreateAccount.py
@@ -7,7 +7,7 @@ class CreateAccount:
         self.ID = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
         print("\nThank you for selecting North Bergen Banking services! \n")
         self.userName = input("Create a username: ")
-        self.userPassword = input("\nCreate a Password: ")
+        self.userPassword = input("Create a Password: ")        #todo make basic password validation
         userPasswordConf = input("Re-enter you Password: ")
 
         while userPasswordConf != self.userPassword:
@@ -23,7 +23,7 @@ class CreateAccount:
         self.lastName = input("What is your last name? ")
         self.MoB = int(input("\nPlease enter your date of birth.\nMonth (MM): "))
         self.DoB = int(input("Day (DD): "))
-        self.YoB = int(input("Year (YYYY): "))
+        self.YoB = int(input("Year (YYYY): "))                  #todo make DOB validation
         self.DOB = (self.MoB, self.DoB, self.YoB)
         self.userBalance = float(input("\nEnter your current balance: $"))
 
@@ -33,3 +33,8 @@ class CreateAccount:
         openF.close()
         print("\nWe have stored your information in our records." \
                 "\nPlease use your ID number '", self.userName, "' to log into your account.")
+
+if __name__ == "__main__":
+    newAcc = CreateAccount()
+    newAcc.fillInfo()
+    newAcc.signUp()
